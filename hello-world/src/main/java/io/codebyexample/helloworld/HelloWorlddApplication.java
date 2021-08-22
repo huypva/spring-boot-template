93d23950f1247b6602347169a156d48db64cee13
huypva <huypva@vng.com.vn>
2021-08-22 08:15:10 +0700

    First commit

---
+++ hello-world/src/main/java/io/codebyexample/helloworld/HelloWorlddApplication.java
@@ -0,0 +1,13 @@
+package io.codebyexample.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorlddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorlddApplication.class, args);
+	}
+
+}
