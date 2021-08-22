f7b241075e3cae407f91054df9483b21e887ed78
huypva <huypva@vng.com.vn>
2021-08-22 11:03:27 +0700

    Rename Application

--- hello-world/src/main/java/io/codebyexample/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/io/codebyexample/helloworld/HelloWorldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorlddApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorlddApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 }
